fd377be8a57aae3b33282e3cca5c637e6cec0d08
qwertz19281 <dellen903@gmail.com>
2026-06-24 19:08:48 +0200

    refactor, license

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ pub mod simulated;
 pub mod parsed_config;
 pub mod button_tracker;
 pub mod event_loop;
+pub mod event_processing;
 
 pub fn init(cfg: &Config) -> anyhow::Result<()> {
     eprintln!("\nConfig: {cfg:#?}");
